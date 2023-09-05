d1d6aa053813bebed88603421276c2746ece01c4
mhmmdglc <muhammed.gulcu@gmail.com>
2023-09-05 14:20:52 +0300

    fixed positions

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,6 +1,6 @@
 function Hero(){
     return(
-        <div className="hero py-32 bg-base-100 absolute">
+        <div className="hero py-32 bg-base-100 relative">
             <div className="hero-content max-w-5xl flex-col lg:flex-row-reverse">
                  <div>
                 <h1 className="text-5xl font-bold">Box Office News!</h1>
